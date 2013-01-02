008548394ddbb19964ea0a31a71e42093207e9c7
zhangyu <zhangyu528@163.com>
2013-01-02 10:10:28 +0800

    add source file and lib about asi-http-request third lib

---
+++ HappyFind/OffLineRes.h
@@ -0,0 +1,13 @@
+//
+//  OffLineRes.h
+//  HappyFind
+//
+//  Created by zzyy on 13-1-2.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OffLineRes : NSObject
+
+@end
---
+++ HappyFind/OffLineRes.m
@@ -0,0 +1,13 @@
+//
+//  OffLineRes.m
+//  HappyFind
+//
+//  Created by zzyy on 13-1-2.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "OffLineRes.h"
+
+@implementation OffLineRes
+
+@end
